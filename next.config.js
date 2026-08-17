0c7ec6f9244a30be8a972fa5ffabac1893f8a7f6
Ikiriza <collingensi5@gmail.com>
2026-08-17 19:26:58 +0300

    New portfolio

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development"
+});
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+  output: "export"
+}
+
+module.exports = withPWA(nextConfig);
